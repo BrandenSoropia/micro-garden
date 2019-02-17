a79bc6c869a83cd5aebfb462d1604b3cb78dd715
Branden Soropia <b.soropia@gmail.com>
2019-02-16 22:05:26 -0500

    Added storybooks. Tested Plant component with it.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import React, { Component } from "react";
 import "./App.css";
 import Plant from "./plant/plant.component";
-import SpriteMapleMature from "./plant/assets/maple-mature.png";
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <Plant
-          type="TREE"
-          name="Maple Tree"
-          sprite={SpriteMapleMature}
-          alt="Tree with thick branches full of green leaves held up by a strong trunk. Beneath the foliage, a light shadow is cast underneath."
-        />
-      </div>
-    );
+    return <div className="App" />;
   }
 }
 
---
+++ src/plant/plant.component.stories.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+
+import Plant from "./plant.component";
+import SpriteMapleMature from "./assets/maple-mature.png";
+
+storiesOf("Plant", module).add("with plant details", () => (
+  <Plant
+    type="TREE"
+    name="Maple Tree"
+    sprite={SpriteMapleMature}
+    alt="Tree with thick branches full of green leaves held up by a strong trunk. Beneath the foliage, a light shadow is cast underneath."
+  />
+));
