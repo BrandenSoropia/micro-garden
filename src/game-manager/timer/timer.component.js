17cecad64f701411b7d6398c3716ce041f005431
Branden Soropia <b.soropia@gmail.com>
2019-03-02 18:24:04 -0500

    Input to set duration.

--- src/game-manager/timer/timer.component.js
+++ src/game-manager/timer/timer.component.js
@@ -1,5 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {
+  MINIMUM_DURATION_LIMIT_MINUTES,
+  MAXIMUM_DURATION_LIMIT_MINUTES
+} from "./duration-presets.constants";
 
 /**
  * Convert milliseconds to minutes including up to 2 decimal places.
@@ -7,9 +11,34 @@ import PropTypes from "prop-types";
 const millisecondsToMinutes = milliseconds =>
   Math.round((milliseconds * 100) / 60000) / 100;
 
-const Timer = ({ startTimer, durationInMilliseconds, clearTimer }) => (
+/**
+ * Convert milliseconds to minutes including up to 2 decimal places.
+ */
+const minutesToMilliseconds = minutes => minutes * 60000;
+
+const Timer = ({
+  startTimer,
+  durationInMilliseconds,
+  clearTimer,
+  setDurationInMilliseconds
+}) => (
   <div>
     <p>{`Duration: ${millisecondsToMinutes(durationInMilliseconds)} mins`}</p>
+    <label htmlFor="duration-in-minutes">Duration:</label>
+    <input
+      id="duration-in-minutes"
+      type="number"
+      min={MINIMUM_DURATION_LIMIT_MINUTES}
+      max={MAXIMUM_DURATION_LIMIT_MINUTES}
+      onChange={e => {
+        e.preventDefault();
+        e.stopPropagation();
+
+        setDurationInMilliseconds(
+          minutesToMilliseconds(parseInt(e.target.value))
+        );
+      }}
+    />
     <button
       onClick={() =>
         startTimer({
