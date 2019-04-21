0b70643b0890d2138c88977d8e173afc220a2d7e
Branden Soropia <b.soropia@gmail.com>
2019-04-21 11:06:05 -0400

    Added proptypes to controls component.

--- src/game-manager/timer/controls/controls.component.js
+++ src/game-manager/timer/controls/controls.component.js
@@ -1,15 +1,16 @@
 import React, { useState } from 'react';
 import { START, PAUSE, CONTINUE } from './controls.constants';
+import PropTypes from 'prop-types';
 
 // TODO: Figure out these states. Should they all be unique? Or can some be reused, like PAUSE??
 const Controls = ({ onStart, onPause, onReset }) => {
   const [state, setState] = useState(PAUSE);
-  const [state, setState] = useState(PAUSE);
 
-  switch (state) {
-    case PAUSE:
-      return <button>START</button>;
-  }
+  return <button type="button">Start</button>;
+};
 
-  return <div>{state === START(<button />)}</div>;
+Controls.propTypes = {
+  onStart: PropTypes.func.isRequired,
+  onPause: PropTypes.func.isRequired,
+  onReset: PropTypes.func.isRequired
 };
